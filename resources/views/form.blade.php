67942ee82c1f30e68ac1cf8d2d15ecc712202ef7
mingjieyan <jeffreyymj@hotmail.com>
2022-04-25 18:10:26 +0800

    update form blade

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -326,7 +326,7 @@
                                                 <div class="col-6">
                                                     <select class="form-control" name="prepaid">
                                                         <option value="首一尾五"> 首一尾五 </option>
-                                                        <option value="全面"> 全面 </option>
+                                                        <option value="全年"> 全年 </option>
                                                         <option value="首一尾三"> 首一尾三 </option>
                                                     </select>
                                                 </div>
